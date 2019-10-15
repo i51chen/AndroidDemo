d9078194e04c200f03274507e93fb29f52f84d15
i51chen <56567729+i51chen@users.noreply.github.com>
2019-10-16 02:30:51 +0800

    1.0
    批量添加监听

--- app/src/main/java/com/example/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/androiddemo/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.androiddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public int[] idArray = {R.id.act1, R.id.act2, R.id.act3, R.id.act4, R.id.act5, R.id.act6, R.id.act7, R.id.act8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < idArray.length; i++) {
+            findViewById(idArray[i]).setOnClickListener(this);
+        }
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == idArray[0]) {
+            Intent intent = new Intent(MainActivity.this, act1.class);
+            startActivity(intent);
+        }
     }
 }
